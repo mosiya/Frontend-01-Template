0be9dc37930d9fdc31ccfa1c6bd1285856a77121
mosiya <shemsy@126.com>
2020-06-04 18:16:46 +0800

    week08 match finished

--- week08/homework/match.js
+++ week08/homework/match.js
@@ -2,83 +2,83 @@ function isSimpleSelectorMatched(selector, element) {
     let attr;
     if(selector.charAt() === '#') {
         attr = element.id;
-        console.log("#", attr, element, selector, attr && attr === selector.slice(1))
-        return attr && attr === selector.slice(1);
-    } else if(selector.charAt() === '.') {
-        attr = [].slice.call(element.classList);
-        console.log(".", attr, element, selector, attr && attr.indexOf(selector.slice(1)) !== -1)
-        return attr && attr.indexOf(selector.slice(1)) !== -1;
-    } else {
-        console.log("tag", element, selector, element.tagName.toLowerCase() === selector)
-        return element.tagName.toLowerCase() === selector
+        if(attr && attr === selector.slice(1)) return element;
     }
+    else if(selector.charAt() === '.') {
+        attr = [].slice.call(element.classList);
+        if(attr && attr.indexOf(selector.slice(1)) !== -1) return element;
+    }
+    else {
+        if(element.tagName.toLowerCase() === selector) return element;
+    }
+    return null;
 }
 
-function isCompoundSelectorMatched(selector, element) {
+function getCompoundSelectorMatchedElement(selector, element) {
+    if(!element || element.nodeType === 9) return null;
     let selectors = selector.split(/(?=[.#])/);
-    return selectors.every(item => isSimpleSelectorMatched(item, element));
+    return selectors.every(item => isSimpleSelectorMatched(item, element)) ? element : null;
 }
-// 这里把对于 和~的处理放到这里去了
-// function _isComplexSelectorMatched(selector, element) {
-//     let selectors = selector.split(/(?<=[ ~])/);
-//     let cur = selectors.pop();
-//     let op = cur.slice(0, 1);
-//     cur = cur.slice(1);
-//     if(!isCompoundSelectorMatched(cur, element)) return false;
-//     while(selectors.length) {
-//         let next = selectors.pop();
-//         if(op === ' ') {
-            
-//             element = element.parentNode;
-//             while(!element || !isCompoundSelectorMatched(next, element)) {
-//               element = element.parentNode;
-//             }
-//             if(!element) return false;
-//         } else if(op === '~') {
-//             let isCurMatched = isCompoundSelectorMatched(cur.slice(1), element);
-//             let next = selectors.pop();
-//             let previousElementSibling = element.previousElementSibling;
-//             let isSiblingMatched = previousElementSibling && _isComplexSelectorMatched(next, previousElementSibling);
-//             console.log("~", isCurMatched, isSiblingMatched, element, previousElementSibling, cur, next)
-//             if(!isCurMatched || !isSiblingMatched) return false;
-//         } else {
-//             console.log("else", cur, element, isCompoundSelectorMatched(cur, element))
-//             if(!isCompoundSelectorMatched(cur, element)) return false;
-//         }
-//     }
-//     return true;
-// }
+
+function  getAncestorMatchedElement(selectorParent, element) {
+    while(element && !getCompoundSelectorMatchedElement(selectorParent, element.parentNode)) {
+        element = element.parentNode;
+    }
+    return element && getCompoundSelectorMatchedElement(selectorParent, element.parentNode) || null;
+}
+
+function  getParentMatchedElement(selectorParent, element) {
+    element = element.parentNode;
+    return element && getCompoundSelectorMatchedElement(selectorParent, element.parentNode);
+}
+
+function  getPrecedingSiblingMatchedElement(selectorSibling, element) {
+    while(element && !getCompoundSelectorMatchedElement(selectorSibling, element.previousElementSibling)) {
+        element = element.previousElementSibling;
+    }
+    return element && getCompoundSelectorMatchedElement(selectorSibling, element.previousElementSibling) || null;
+}
+
+function  getPreviousSiblingMatchedElement(selectorSibling, element) {
+    element = element.previousElementSibling
+    return element && getCompoundSelectorMatchedElement(selectorSibling, element);
+}
+
 function removeExtraSpace(selector) {
-    return selector.replace(/\s+([>+~])\s+/, '$1').replace(/\s+/g, ' ');
+    return selector.replace(/\s*([>+~])\s*/, '$1').replace(/\s+/g, ' ');
 }
 
-function isComplexSelectorMatched(selector, element) {
-    selector = removeExtraSpace(selector);
-    let selectors = selector.split(/(?<=[>+])/).reverse();
-    return selectors.every(item => {
-        if(item.charAt() === '>') {
-            let isMatched = _isComplexSelectorMatched(item.slice(1), element);
-            element = element.parentNode;
-            console.log(">", isMatched, element, item);
-            return element && isMatched;
-        }
-        if(item.charAt() === '+') {
-            let isMatched = _isComplexSelectorMatched(item.slice(1), element);
-            element = element.previousElementSibling;
-            console.log("+", isMatched, element, item);
-            return element && isMatched;
-        }
+function getComplexSelectorMatchedElement(selectors, element) {
+    if(selectors.length < 2) return getCompoundSelectorMatchedElement(selectors.pop(), element);
 
-        console.log("else else", element, item)
-        return _isComplexSelectorMatched(item, element);
-    })
+    let combinator = selectors[1];
+    let selector = selectors.shift();
+        selectors.shift();
+    if(combinator === ' ') {
+        element = getComplexSelectorMatchedElement(selectors, element);
+        return getAncestorMatchedElement(selector, element);
+    }
+    else if(combinator === '~') {
+        element = getComplexSelectorMatchedElement(selectors, element);
+        return getPrecedingSiblingMatchedElement(selector, element);
+    }
+    else if(combinator === '>') {
+        element = getComplexSelectorMatchedElement(selectors, element);
+        return getParentMatchedElement(selector, element);
+    }
+    else if(combinator === '+') {
+        element = getComplexSelectorMatchedElement(selectors, element);
+        return getPreviousSiblingMatchedElement(selector, element);
+    }
 }
 
 function isMatched(selector, element) {
     if(!selector || !element) return false;
+    selector = removeExtraSpace(selector);
+    let selectors = selector.split(/(?=[ >~+])|(?<=[[ >~+])/g);
 
-    return isComplexSelectorMatched(selector, element);
+    return !!getComplexSelectorMatchedElement(selectors, element);
 }
  
  
-console.log(isMatched("body   div     span   ~  span#id", document.getElementById("id")));
+console.log(isMatched("body  div   div + span#id", document.getElementById("id")));
